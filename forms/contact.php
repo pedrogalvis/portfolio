616b070985b15407000f7c3abfbc6cf1c351cdc2
peterg <97988826+pedrogalvis@users.noreply.github.com>
2024-09-03 18:48:03 -0500

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
 		$from = $email; 
 		
 		// WARNING: Be sure to change this. This is the address that the email will be sent to
-		$to = 'YOUR_SPECIFIED_EMAIL_ADDRESS'; 
+		$to = 'pedro.galvis.talento.tech@usa.edu.co'; 
 		
 		$subject = "Message from ".$name." ";
 		
